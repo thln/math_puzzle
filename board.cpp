565049f3fc7699ec92399a089cc2f41bb66da83c
Tam Nguyen <tamnguye@usc.edu>
2013-03-26 20:28:02 -0700

    Added some member functions

--- board.cpp
+++ board.cpp
@@ -8,6 +8,17 @@
 using namespace std;
 
 
+// Default constructor
+Board::Board()
+{
+ size_ = 4;
+ tiles_ = new int[4];
+ for(int i = 0; i< size_; i++)
+ {
+ 	tiles_[i] = i;
+ }
+}
+
 /** Init a board of given size and scramble it with numInitMoves 
  * by moving the space tile with a randomly chosen direction N, W, S, E
  * some of which may be invalid, in which case we skip that move 
@@ -63,20 +74,98 @@ Board::Board(int size, int numInitMoves, int seed )
 }
 
 
-/** Default constructor. provide documentation here */
-Board::Board()
-{
-  /*
-   * IMPLEMENT ME
-  */
-}
-
-/** Default destructor. provide documentation here */
+// Default destructor
 Board::~Board()
 {
-  /*
-   * IMPLEMENT ME
-  */
+	  delete [] tiles_;
+}
+
+//Copy Constructor #1
+Board::Board(const Board &b)
+{
+	tiles_ = new int[b.size_];
+	//*tiles_ = *(b.tiles_);
+	for(int i=0; i<b.size_; i++)
+	{
+		tiles_[i] = b.tiles_[i];
+	}
+}
+
+//Copy Constructor #2
+Board::Board(int *tiles, int size)
+{
+	tiles_ = new int[size];
+	for(int i=0; i<size; i++)
+	{
+		tiles_[i] = tiles[i];
+	}
+}
+
+//Swaps the blank with the specified tile
+void Board::move(int tile)
+{
+	//checking if tile is in the game or not
+	if(tile<0 || tile>(size_-1))
+	{
+		cout << "Tile does not exist." << endl;
+	}
+	else{
+
+/* faster way to do shown below
+	//finding specified tile
+	for(int i=0; i<size_; i++)
+	{
+	   if(tile == tiles_[i])
+	   {
+	      //finding the zero or "empty" tile
+	      for(int j=0; j<size_; j++)
+	      {
+		if(tiles[j] == 0)
+		{
+		//swapping tiles
+		  tiles[j] = tile;
+		  tiles[i] = 0;
+		  break;
+		}		
+	      }
+	   }
+	}
+*/
+	
+	//looping through all tiles
+	//variables keeping track of tiles to swap
+	int a, b;
+	for(init i=0; i<size_; i++)
+	{
+		//finding shown tile
+		if(tile == tiles_[i])
+		{
+		a = i;
+		}
+		//finding zero tile
+		if(0 == tiles_[i])
+		{
+		b = i;
+		}
+		
+	}
+	tiles_[a] = 0;
+	tiles_[b] = tile;
+	}
+}
+
+//Checks if board is solved
+bool Board::solved()
+{
+	for(int i=0; i<size_; i++)
+	{
+	   if(tiles_[i] != i)
+	   {
+	     return false;
+	   }
+	}
+
+  return true;
 }
 
 bool Board::operator<(const Board& rhs) const
